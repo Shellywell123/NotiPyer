956194052bf1e6e4e4f0f1b7d125af1bac660eb1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 12:21:48 +0100

    added sandisk.py

--- sandisk.py
+++ sandisk.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver import Firefoxpoke
-
+from selenium.webdriver import Firefox
 
 import bs4
+import json
 
 ff_opts = Options()
 ff_opts.add_argument("--headless")
 
 driver = webdriver.Firefox(options=ff_opts, executable_path="./geckodriver")
+
 
 product_url = "https://www.amazon.co.uk/SanDisk-Ultra-Flash-Drive-Read/dp/B083ZS4HYD/ref=psdc_430554031_t1_B07NS1Y9K3?th=1"
 
@@ -25,12 +26,28 @@ driver.quit()
 bs = bs4.BeautifulSoup(pagesrc, "html.parser")
 
 # filter buttons
-price_buttons = bs.findAll(lambda tag: tag.name == "li" and "id" in tag.attrs and "size_name_" in tag.attrs["id"])
+price_buttons = bs.findAll(
+    lambda tag: tag.name == "li"
+    and "id" in tag.attrs
+    and "size_name_" in tag.attrs["id"]
+)
 
-sizes = [
-    tag.find("p").text for tag in price_buttons
-]
+
+if price_buttons:
+
+    sizes = [tag.find("p").text for tag in price_buttons]
+
+else:
+
+    datatag = bs.findAll(
+        lambda tag: tag.name == "script"
+        and "data-a-state" in tag.attrs
+        and "dim-val-list" in tag.attrs["data-a-state"]
+    )
+
+    content = json.loads(datatag[0].contents[0])
+    sizes = content["size_name"]
 
 print("Available sizes are:")
-for i in sizes: 
+for i in sizes:
     print("  - ", i)
